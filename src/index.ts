0bca712fb9caa01dd815b8a53d6361f5c78faae7
Murilo Henrique <oofleaoo@gmail.com>
2020-08-09 19:08:59 -0300

    feat: add .env properties

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
 
 const app = express();
 
+dotenv.config();
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log(`Servidor rodando na porta ${3333}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Servidor rodando na porta ${process.env.PORT}`)
+);
